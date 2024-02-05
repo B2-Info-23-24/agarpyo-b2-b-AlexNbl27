d24331f9c1ddf92f2dc96c4ea534a204d1bdf50e
Alexandre NOBLET <alexandre.noblet@ynov.com>
2024-02-05 21:02:10 +0100

    Implement the main classes. First working version

---
+++ App/settings.py
@@ -0,0 +1,4 @@
+class Settings :
+    def __init__(self) :
+        self.allmodes = {"mouse": 0, "keyboard": 1}
+        self.alllevels = {"easy": 2, "medium": 3, "hard": 4}
---
+++ App/trap.py
@@ -0,0 +1,11 @@
+import random
+from object import Object
+
+
+class Trap(Object):
+    def __init__(self, screen):
+        _radius = random.randint(40, 150)
+        _color = (0, 0, 255)
+        _x = random.randint(0, screen.get_width())
+        _y = random.randint(0, screen.get_height())
+        super().__init__(_radius, _color, _x, _y)
